c420510d7494b7b38aaf79799b28f1d7533ae71b
AntonioJ-Perez <gccantonio.c@gmail.com>
2015-06-22 13:06:20 -0500

    prueba panel  gestor

--- src/java/util/constantes/Directorios.java
+++ src/java/util/constantes/Directorios.java
@@ -10,5 +10,5 @@ package util.constantes;
  * @author brionvega
  */
 public interface Directorios {
-    public static final String RUTA_IMAGENES_DE_PERFIL = "/img/usuarios/";
+    public static final String RUTA_IMAGENES_DE_PERFIL = "/usuarios/";
 }
