a5284139709be3d6bcf5d14d6325542361445522
chavez92 <chavez.eduardo.08.07@gmail.com>
2015-07-22 10:32:37 -0500

    Tarea 2 SCRUM. Modulo empresas. Se comienza a trabajar con el Bean de empresas

--- src/java/beans/EmpresasBean.java
+++ src/java/beans/EmpresasBean.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package beans;
+
+import dao.EmpresasDAO;
+import dao.SujetosDAO;
+import dto.Empresas;
+import dto.Sujetos;
+import impl.EmpresasIMPL;
+import impl.SujetosIMPL;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 /**
  *
  * @author Eduardo
  */
+@ManagedBean(name = "empresasBean")
+@ViewScoped
 public class EmpresasBean {
-    
+    private Empresas empresa;
+    private Sujetos sujeto;
 }
