abdeed061ba742e127f44f3fd9ffbdb0fd7d0ea1
chavez92 <chavez.eduardo.08.07@gmail.com>
2015-07-22 13:32:32 -0500

    Tarea 2 SCRUM. Modulo empresas.Primera aproximacion del modulo completo. No carga la busqueda y no funcionan los cuadros de dialogo.

--- src/java/beans/EmpresasBean.java
+++ src/java/beans/EmpresasBean.java
@@ -1,14 +1,14 @@
 package beans;
 
 import dao.EmpresasDAO;
-import dao.SujetosDAO;
 import dto.Empresas;
-import dto.Sujetos;
 import impl.EmpresasIMPL;
-import impl.SujetosIMPL;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -16,7 +16,65 @@ import javax.faces.bean.ViewScoped;
  */
 @ManagedBean(name = "empresasBean")
 @ViewScoped
-public class EmpresasBean {
+public class EmpresasBean implements Serializable{
+
     private Empresas empresa;
-    private Sujetos sujeto;
+    private EmpresasDAO empresaDao;
+    private String nombreCorto;
+    private String rfc;
+    private List<Empresas> listaEmpresas;
+
+    public EmpresasBean() {
+        empresaDao = new EmpresasIMPL();
+        empresa = new Empresas();
+        listaEmpresas = empresaDao.buscarTodo();
+        int i;
+        System.out.println("************ CONSOLA SIGERWEB ****************");
+        System.out.println("Empresas registradas en el sistema:");
+        for(i=0; i<(listaEmpresas.size()); i++)
+        {
+            System.out.println(listaEmpresas.get(i).getNombreCorto());
+        }
+    }
+    
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public Empresas getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresas empresa) {
+        this.empresa = empresa;
+    }
+
+    public EmpresasDAO getEmpresaDao() {
+        return empresaDao;
+    }
+
+    public void setEmpresaDao(EmpresasDAO empresaDao) {
+        this.empresaDao = empresaDao;
+    }
+
+    public String getNombreCorto() {
+        return nombreCorto;
+    }
+
+    public void setNombreCorto(String nombreCorto) {
+        this.nombreCorto = nombreCorto;
+    }
+
+    public List<Empresas> getListaEmpresas() {
+        return listaEmpresas;
+    }
+
+    public void setListaEmpresas(List<Empresas> listaEmpresas) {
+        this.listaEmpresas = listaEmpresas;
+    }
+
 }
