828437ee2460f61a33d218d657bd499840c68381
chavez92 <chavez.eduardo.08.07@gmail.com>
2015-08-11 13:07:27 -0500

    Merge origin/usuariosBranch into usuariosBranch
    Conflicts:
    	src/java/beans/EmpresasBean.java

--- src/java/beans/EmpresasBean.java
+++ src/java/beans/EmpresasBean.java
@@ -172,6 +172,51 @@ public class EmpresasBean implements Serializable {
         }
     }
     
+    public void crearProducto() {
+        nuevoProducto.setNombre(nuevoProd);
+        nuevoProducto.setDescripcion(nuevaDesc);
+        okNuevoProducto = productoDao.insertar(nuevoProducto);
+        if(okNuevoProducto){
+        System.out.println("************ CONSOLA SIGERWEB ****************");
+        System.out.println("Se registro el producto " + nuevoProd + " exitosamente");
+        /*
+        RequestContext.getCurrentInstance().update("formNuevaEmpresa");
+        RequestContext.getCurrentInstance().update("formEditarEmpresa");
+        */
+        }
+        else{
+            System.out.println("Error fatal. No se registro el producto " + nuevoProd + " en el sistema");
+        }
+    }
+
+    public void editarProducto() {
+        System.out.println("************ CONSOLA SIGERWEB ****************");
+        System.out.println("Se quiere editar el producto " + prod);
+        RequestContext.getCurrentInstance().update("editarProductos");
+    }
+    
+    public void crearSubproducto() {
+        nuevoSubproducto.setNombre(nuevoSubprod);
+        nuevoSubproducto.setDescripcion(nuevaSubdesc);
+        nuevoSubproducto.setProducto(seleccionadoCombobox);
+        okNuevoSubproducto = subproductoDao.insertar(nuevoSubproducto);
+        if(okNuevoSubproducto){
+        System.out.println("************ CONSOLA SIGERWEB ****************");
+        System.out.println("Se registro el subproducto " + nuevoSubprod + " exitosamente");
+        /*
+        RequestContext.getCurrentInstance().update("formNuevaEmpresa");
+        RequestContext.getCurrentInstance().update("formEditarEmpresa");
+        */
+        }
+        else{
+            System.out.println("Error fatal. No se registro el producto " + nuevoSubprod + " en el sistema");
+        }
+    }
+
+    public void editarSubproducto() {
+        System.out.println("************ CONSOLA SIGERWEB ****************");
+        System.out.println("Se quiere editar el subproducto " + subprod);
+        RequestContext.getCurrentInstance().update("editarSubproductos");
     }
 
     /**
@@ -546,55 +591,6 @@ public class EmpresasBean implements Serializable {
         this.okEditarSubproducto = okEditarSubproducto;
     }
 
-
-}
-    public void crearProducto() {
-        nuevoProducto.setNombre(nuevoProd);
-        nuevoProducto.setDescripcion(nuevaDesc);
-        okNuevoProducto = productoDao.insertar(nuevoProducto);
-        if(okNuevoProducto){
-        System.out.println("************ CONSOLA SIGERWEB ****************");
-        System.out.println("Se registro el producto " + nuevoProd + " exitosamente");
-        /*
-        RequestContext.getCurrentInstance().update("formNuevaEmpresa");
-        RequestContext.getCurrentInstance().update("formEditarEmpresa");
-        */
-        }
-        else{
-            System.out.println("Error fatal. No se registro el producto " + nuevoProd + " en el sistema");
-        }
-    }
-
-    public void editarProducto() {
-        System.out.println("************ CONSOLA SIGERWEB ****************");
-        System.out.println("Se quiere editar el producto " + prod);
-        RequestContext.getCurrentInstance().update("editarProductos");
-    }
-    
-    public void crearSubproducto() {
-        nuevoSubproducto.setNombre(nuevoSubprod);
-        nuevoSubproducto.setDescripcion(nuevaSubdesc);
-        nuevoSubproducto.setProducto(seleccionadoCombobox);
-        okNuevoSubproducto = subproductoDao.insertar(nuevoSubproducto);
-        if(okNuevoSubproducto){
-        System.out.println("************ CONSOLA SIGERWEB ****************");
-        System.out.println("Se registro el subproducto " + nuevoSubprod + " exitosamente");
-        /*
-        RequestContext.getCurrentInstance().update("formNuevaEmpresa");
-        RequestContext.getCurrentInstance().update("formEditarEmpresa");
-        */
-        }
-        else{
-            System.out.println("Error fatal. No se registro el producto " + nuevoSubprod + " en el sistema");
-        }
-    }
-
-    public void editarSubproducto() {
-        System.out.println("************ CONSOLA SIGERWEB ****************");
-        System.out.println("Se quiere editar el subproducto " + subprod);
-        RequestContext.getCurrentInstance().update("editarSubproductos");
-    }
-
     public Subproducto getSubproduct() {
         return subproduct;
     }
@@ -649,3 +645,5 @@ public class EmpresasBean implements Serializable {
 
     public void setId(int id) {
         this.id = id;
+    }
+}
