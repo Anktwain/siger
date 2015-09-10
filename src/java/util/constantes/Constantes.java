785d035e8e0801106b2d05091ffea69d7cf1626b
pabloBlues <planderos@live.com.mx>
2015-09-10 10:20:50 -0500

    Inclusion de FilaBean, del DTO Fila.

--- src/java/util/constantes/Constantes.java
+++ src/java/util/constantes/Constantes.java
@@ -15,4 +15,17 @@ public interface Constantes {
    *
    */
   public final int longPassword = 20;
+
+  public final int limCREDITO_numero_credito = 30;
+  public final int limSUJETO_nombre_razon_social = 100;
+  public final int limLINEA_telefono = 30;
+  public final int limREMESA_estatus = 10;
+  public final int limCREDITO_numero_cuenta = 30;
+  public final int limCLIENTE_numero_cliente = 25;
+  public final int limSUJETO_rfc = 15;
+  public final int limDIRECCION_calle = 150;
+  public final int limCOLONIA_nombre = 100;
+  public final int limESTADO_REPUBLICA_nombre = 50;
+  public final int limMUNICIPIO_nombre = 50;
+  public final int limMUNICIPIO_cp = 5;
 }
