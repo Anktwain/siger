d3cd471f5437ddfaf914931e72061b87c04d07a2
pabloBlues <planderos@live.com.mx>
2015-09-10 13:35:01 -0500

    Nuevos validadores (incompleto), nuevas constantes y nuevos mensajes de error.

--- src/java/beans/FilaBean.java
+++ src/java/beans/FilaBean.java
@@ -2,6 +2,8 @@ package beans;
 
 import dto.Fila;
 import java.io.Serializable;
+import util.constantes.Constantes;
+import util.constantes.Errores;
 
 /**
  *
@@ -22,23 +24,91 @@ public class FilaBean implements Serializable {
   public void setFilaActual(Fila filaActual) {
     this.filaActual = filaActual;
   }
+
   /**
-   * 
+   * El número de crédito es obligatorio.
    */
-  public void validaNumCred(String numCred) throws Exception{
-    if (!this.filaActual.getCredito().isEmpty()){
-      if(this.filaActual.getCredito() > ){
+  public void validaNumCred() throws Exception {
+    if (!this.filaActual.getCredito().isEmpty()) {
+      if (this.filaActual.getCredito().length() > Constantes.limCREDITO_numero_credito) {
+        throw new Exception("Error en el campo \"numero_credito\". "
+                + Errores.campoDesbordado + " (máx: " + Constantes.limCREDITO_numero_credito + ".)");
       }
-    }else{
-      throw new Exception("El campo se encuentra vacío");
+    } else {
+      throw new Exception("Error en el campo \"numero_credito\". " + Errores.campoVacio);
+    }
+  }
+
+  /**
+   * El nombre del deudor es obligatorio.
+   */
+  public void validaNombreRazonSoc() throws Exception {
+    if (!this.filaActual.getNombre().isEmpty()) {
+      if (this.filaActual.getNombre().length() > Constantes.limSUJETO_nombre_razon_social) {
+        throw new Exception("Error en el campo \"nombre_razon_social\". "
+                + Errores.campoDesbordado + " (máx: " + Constantes.limSUJETO_nombre_razon_social + ".)");
+      }
+    } else {
+      throw new Exception("Error en el campo \"nombre_razon_social\". " + Errores.campoVacio);
+    }
+  }
+
+  /**
+   * La refCobro (referencia de cobro) es obligatoria.
+   */
+  public void validaRefCobro() throws Exception {
+    if (!this.filaActual.getRefCobro().isEmpty()) {
+      if (this.filaActual.getRefCobro().length() > Constantes.limLINEA_telefono) {
+        throw new Exception("Error en el campo \"linea_telefono\". "
+                + Errores.campoDesbordado + " (máx: " + Constantes.limLINEA_telefono + ".)");
+      }
+    } else {
+      throw new Exception("Error en el campo \"linea_telefono\". " + Errores.campoVacio);
+    }
+  }
+
+  /**
+   * La linea(producto) es obligatoria, deberá comprobarse que existe el
+   * producto en la tabla 'producto'
+   */
+  public void validaIdProducto() throws Exception {
+    if (!this.filaActual.getLinea().isEmpty()) {
+      try {
+        Integer.parseInt(this.filaActual.getLinea());
+      } catch (NumberFormatException nfe) {
+        throw new Exception("Error en el campo \"productos_id_producto\". " + Errores.campoEnteroRequerido);
+      }
+    } else {
+      throw new Exception("Error en el campo \"productos_id_producto\". " + Errores.campoVacio);
+    }
+  }
+
+  /**
+   * El tipoCredito(subproducto) deberá existir en la tabla 'subproducto', es un
+   * id entero, sin embargo este campo no es obligatorio.
+   */
+  public void validaIdSubproducto() throws Exception {
+    if (!this.filaActual.getTipoCredito().isEmpty()) {
+      try {
+        Integer.parseInt(this.filaActual.getTipoCredito());
+      } catch (NumberFormatException nfe) {
+        throw new Exception("Error en el campo \"subproductos_id_subproducto\". " + Errores.campoEnteroRequerido);
+      }
+    }
+  }
+
+  /**
+   * El estatus no es obligatorio. Es un VARCHAR(10) // remesa.estatus
+   */
+  public void validaEstatus() throws Exception {
+    if (!this.filaActual.getEstatus().isEmpty()) {
+      if (this.filaActual.getEstatus().length() > Constantes.limREMESA_estatus) {
+        throw new Exception("Error en el campo \"estatus\". "
+                + Errores.campoDesbordado + " (máx: " + Constantes.limREMESA_estatus + ".)");
+      }
     }
   }
   
-  /**
-   * 
-   */
-  public void valida(String s){
-    
-  }
+  
 
 }
---
+++ src/java/util/constantes/Errores.java
@@ -0,0 +1,12 @@
+
+package util.constantes;
+
+/**
+ *
+ * @author Pablo
+ */
+public interface Errores {
+  String campoVacio = "El campo no debe encontrarse vacío.";
+  String campoDesbordado = "El campo excede el límite de caracteres permitidos.";
+  String campoEnteroRequerido = "El campo no contiene un número entero o rebasa los límites del tipo int(4 bytes).";
+}
