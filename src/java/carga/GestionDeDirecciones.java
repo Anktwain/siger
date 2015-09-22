badf50be03510d28fd2234fc24e8d74054cac3b7
chavez92 <chavez.eduardo.08.07@gmail.com>
2015-09-22 11:26:35 -0500

    Version estable para gestion de direcciones para validar direcciones de la remesa por codigo postal

--- src/java/carga/GestionDeDirecciones.java
+++ src/java/carga/GestionDeDirecciones.java
@@ -20,9 +20,9 @@ public class GestionDeDirecciones {
     // CREAMOS UNA LISTA QUE CONTENDRA LOS DATOS VERIFICADOS SI ES QUE LOS ENCUENTRA
     List<String> verificados = new ArrayList<>();
     // VARIABLES PARA POSTERIORES CONSULTAS
-    String municipio = "1";
-    String colonia = "1";
-    String estado = "1";
+    String municipio = "0";
+    String colonia = "0";
+    String estado = "0";
     // INICIAMOS UNA CONEXION A BASE DE DATOS
     try {
       DriverManager.registerDriver(new com.mysql.jdbc.Driver());
@@ -30,36 +30,27 @@ public class GestionDeDirecciones {
       Statement consulta = conexion.createStatement();
       // PARA OBTENER EL MUNICIPIO
       String query = "SELECT id_municipio_municipios FROM colonia WHERE codigo_postal = '" + cp + "' LIMIT 1;";
-      System.out.println(query);
       ResultSet r = consulta.executeQuery(query);
       while(r.next()) {
         municipio = r.getString("id_municipio_municipios");
-        System.out.println("ID MUNICIPIO: " + municipio);
       }
       // PARA OBTENER EL ESTADO
       query = "SELECT id_estado_estados FROM municipio WHERE id_municipio = " + municipio + ";";
-      System.out.println(query);
       r = consulta.executeQuery(query);
       while(r.next()) {
         estado = r.getString("id_estado_estados");
-        System.out.println("ID ESTADO: " + estado);
       }
       // PARA OBTENER LA COLONIA
       // CABE RESALTAR QUE EN CASO DE QUE LA BUSQUEDA ARROJE MAS DE UN RESULTADO, SE TOMARA EN CUENTA SOLO EL PRIMERO
       query = "SELECT id_colonia FROM colonia WHERE codigo_postal = '" + cp + "' AND (SELECT CONCAT(tipo, ' ', nombre) FROM colonia WHERE codigo_postal = '" + cp + "' LIMIT 1) LIKE (SELECT CONCAT('', '%', tipo, ' ', nombre, '%', '') FROM colonia WHERE codigo_postal = '" + cp + "' LIMIT 1) LIMIT 1;";
-      System.out.println(query);
       r = consulta.executeQuery(query);
       while(r.next()) {
         colonia = r.getString("id_colonia");
-        System.out.println("ID COLONIA: " + colonia);
       }
       // AGREGAMOS A LA LISTA
       verificados.add(estado);
       verificados.add(municipio);
       verificados.add(colonia);
-    } 
-    catch (Exception e) {
-      e.printStackTrace();
     }
     finally {
       return verificados;
