77ae4e02b1e71c2222a5d60c0a9bd113520f9cca
pabloBlues <planderos@live.com.mx>
2015-10-09 11:30:39 -0500

    Sincronización con la versión de Lalo <3 <3

--- src/java/carga/asignacion/Asignacion.java
+++ src/java/carga/asignacion/Asignacion.java
@@ -15,8 +15,8 @@ public class Asignacion {
   private ArrayList<ArrayList<String>> gestores;
 
   /**
-   * Ordena {@code nuevosTotales} de mayor a menor con base en el monto de su saldo
-   * vencido.
+   * Ordena {@code nuevosTotales} de mayor a menor con base en el monto de su
+   * saldo vencido.
    */
   public void ordenarDecreceiente() {
     Collections.sort(nuevosTotales, new Comparator<Fila>() {
@@ -32,19 +32,52 @@ public class Asignacion {
       }
     });
   }
-/**
- * 
- */
+
+  /**
+   *
+   */
   public void asignarNuevosTotales() {
-   
     /**
-     * 
-     *
+     * Calculamos el número de iteraciones. Por iteracion se reparten dos
+     * créditos a cada gestor.
      */
-    
     int iteraciones = this.nuevosTotales.size() / (2 * gestores.size());
 
+    /**
+     * Se reparten de a dos en dos los créditos entre los gestores, dandole a
+     * cada uno el mayor y el menor dispoinibles en cada iteración. Pueden
+     * sobrar hasta (2*n)-1 creditos por repartir.
+     */
     for (int i = 0; i < iteraciones; i++) {
+
+    }
+
+    int restantes = this.nuevosTotales.size() % (2 * gestores.size());
+
+    if (restantes > 0) {
+      ArrayList<Fila> disponibles = new ArrayList<Fila>();
+      for (int i = 0; i < restantes; i++) {
+        disponibles.add(nuevosTotales.get(iteraciones + i));
+      }
+      if (restantes % gestores.size() != 0) {
+        /**
+         * Se reparte una vez más entre todos los gestores, ahora comenzando por
+         * el último en la lista. Pueden sobrar hasta n-1 creditos por repartir.
+         */
+        for (int i = 0; i < gestores.size(); i++) {
+
+          disponibles.remove();
+        }
+      } else if (restantes > 0) {
+        Collections.min(gestores, new Comparator<ArrayList<String>>() {
+
+          @Override
+          public int compare(ArrayList<String> o1, ArrayList<String> o2) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          }
+
+        });
+      }
 
     }
 
