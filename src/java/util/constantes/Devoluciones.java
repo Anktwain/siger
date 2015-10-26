f7abd09f417fd3cc1c4b971b862f7d9862d52ce4
chavez92 <chavez.eduardo.08.07@gmail.com>
2015-10-26 11:47:17 -0600

    Primera aproximacion al modulo devoluciones

---
+++ src/java/util/constantes/Devoluciones.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.constantes;
+
+/**
+ *
+ * @author Eduardo
+ */
+
+// INTERFAZ PARA DECLARA LAS CONSTANTES UTILIZADAS PARA CONOCER EL ESTADO DE LOS CREDITOS DEVUELTOS
+public interface Devoluciones {
+  
+  public static final int PENDIENTE = 2;
+  
+  public static final int ESPERA_CONSERVACION = 3;
+  
+  public static final int DEVUELTO = 0;
+  
+  public static final int CONSERVADO = 1;
+  
+}
