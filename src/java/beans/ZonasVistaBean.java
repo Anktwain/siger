906836093327e8749c7ec1862d97d3c3fc6ff579
pabloBlues <planderos@live.com.mx>
2015-10-29 12:40:07 -0600

    Actualización de la vista Zonas y su respectivo bean. Creacion del DTO y DAO Zonas. Inclusion de los DTOs, DAOs e IMPLs de EstadoRep, Municipio y Colonia en la clase Zona. Inclusion del metodo toString en los DTOs Municipio, Colonia y EstadoRep.

--- src/java/beans/ZonasVistaBean.java
+++ src/java/beans/ZonasVistaBean.java
@@ -1,5 +1,12 @@
 package beans;
 
+import dao.EstadoRepublicaDAO;
+import dao.MunicipioDAO;
+import dto.Colonia;
+import dto.EstadoRepublica;
+import dto.Municipio;
+import impl.EstadoRepublicaIMPL;
+import impl.MunicipioIMPL;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,117 +22,233 @@ import util.log.Logs;
 @ViewScoped
 public class ZonasVistaBean implements Serializable {
 
+  /**
+   * El nombre de la zona con el que se le identificará en su respectivo
+   * despacho de creación. El nombre de zona es <strong> único por despacho
+   * </strong>, pero dos despachos cualesquiera podrían tener zonas con los
+   * mismos nombres.
+   */
   private String nombre;
-  
-  private String estadoSeleccionado;
-  
-  private List<String> estadosDePrueba;
-  private List<String> mpiosDePrueba;
-  private List<String> coloniasDePrueba;
 
-  private List<String> estadosAutoCompletados;
-  private List<String> mpiosAutoCompletados;
-  private List<String> coloniasAutoCompletadas;
+  /* **** Código de prueba *****/
+//  private String estadoSeleccionado;
+//
+//  private List<String> estadosDePrueba;
+//  private List<String> mpiosDePrueba;
+//  private List<String> coloniasDePrueba;
+//
+//  private List<String> estadosAutoCompletados;
+//  private List<String> mpiosAutoCompletados;
+//  private List<String> coloniasAutoCompletadas;
+//
+//  private List<String> estadosSeleccionados;
+//  private List<String> mpiosSeleccionados;
+//  private List<String> coloniasSeleccionadas;
+  /* **** Código de prueba *****/
+  
+  /**
+   * Todos los estadosRep que se listarán en la vista.
+   */
+  private List<EstadoRepublica> estadosRep;
+  /**
+   * Todos los municipios que se desplegarán en la vista y que corresponden a
+   * los del estado desplegado.
+   */
+  private List<Municipio> mpiosVisibles;
+  /**
+   * Todas las colonias que se desplegarán en la vista y que corresponden a los
+   * del municipio desplegado.
+   */
+  private List<Colonia> coloniasVisibles;
 
-  private List<String> estadosSeleccionados;
-  private List<String> mpiosSeleccionados;
-  private List<String> coloniasSeleccionados;
+  /**
+   * Lista con las coincidencias encontradas que se desplegará cuando el usuario
+   * introduzca un criterio de búsqueda de municipios en la vista.
+   */
+  private List<Municipio> mpiosAutoCompletados;
+  /**
+   * Lista con las coincidencias encontradas que se desplegará cuando el usuario
+   * introduzca un criterio de búsqueda de colonias en la vista.
+   */
+  private List<Colonia> coloniasAutoCompletadas;
+
+  /**
+   * Lista en la que se almacenan todos los municipios seleccionados de todos
+   * los estadosRep.
+   */
+  private List<Municipio> mpiosSeleccionados;
+  /**
+   * Lista en la que se almacenan todas las colonias seleccionadas de todos los
+   * municipios.
+   */
+  private List<Colonia> coloniasSeleccionadas;
+
+  /**
+   * Objeto DAO para realizar las operaciones en la base de datos que
+   * corresponden con la tabla Municipio.
+   */
+  private MunicipioDAO mpioDao;
+  /**
+   * Lista donde se almacenan todos los estadosRep de la república seleccionados
+   * en la vista.
+   */
+  private List<EstadoRepublica> edosRepSeleccionados;
+  /**
+   * Estado que actualmente se despliega en la vista.
+   */
+  private EstadoRepublica edoRepVisible;
+  /**
+   * Número que corresponde al orden en la lista en el que se encuentra el
+   * estado que actualmente se despliega en la vista.
+   */
+  private int idEdoVisible;
+
+  private List<Municipio> mpiosDelEstadoRepSelec;
+  private List<Colonia> coloniasDelEstadoRepSelec;
 
   public ZonasVistaBean() {
 
-    estadosDePrueba = new ArrayList<>();
-    mpiosDePrueba = new ArrayList<>();
-    coloniasDePrueba = new ArrayList<>();
-
-    estadosAutoCompletados = new ArrayList<>();
-    mpiosAutoCompletados = new ArrayList<>();
-    coloniasAutoCompletadas = new ArrayList<>();
-
-    estadosSeleccionados = new ArrayList<>();
-    mpiosSeleccionados = new ArrayList<>();
-    coloniasSeleccionados = new ArrayList<>();
-
-    estadosDePrueba.add("Aguascalientes");
-    estadosDePrueba.add("Chiapas");
-    estadosDePrueba.add("Distrito Federal");
-    estadosDePrueba.add("Querétaro");
-    estadosDePrueba.add("Zacatecas");
-
-    mpiosDePrueba.add("Álvaro Obregón");
-    mpiosDePrueba.add("Benito Juárez");
-    mpiosDePrueba.add("Cuauhtémoc");
-    mpiosDePrueba.add("Milpa Alta");
-    mpiosDePrueba.add("Venustiano Carranza");
+    /* **** Código de prueba *****/
+//    estadosDePrueba = new ArrayList<>();
+//    mpiosDePrueba = new ArrayList<>();
+//    coloniasDePrueba = new ArrayList<>();
+//
+//    estadosAutoCompletados = new ArrayList<>();
+//    mpiosAutoCompletados = new ArrayList<>();
+//    coloniasAutoCompletadas = new ArrayList<>();
+//
+//    estadosSeleccionados = new ArrayList<>();
+//    mpiosSeleccionados = new ArrayList<>();
+//    coloniasSeleccionadas = new ArrayList<>();
+//
+//    estadosDePrueba.add("Aguascalientes");
+//    estadosDePrueba.add("Chiapas");
+//    estadosDePrueba.add("Distrito Federal");
+//    estadosDePrueba.add("Querétaro");
+//    estadosDePrueba.add("Zacatecas");
+//
+//    mpiosDePrueba.add("Álvaro Obregón");
+//    mpiosDePrueba.add("Benito Juárez");
+//    mpiosDePrueba.add("Cuauhtémoc");
+//    mpiosDePrueba.add("Milpa Alta");
+//    mpiosDePrueba.add("Venustiano Carranza");
+//
+//    coloniasDePrueba.add("Obrera");
+//    coloniasDePrueba.add("Doctores");
+//    coloniasDePrueba.add("Tránsito");
+//    coloniasDePrueba.add("Pro hogar");
+//    coloniasDePrueba.add("Anáhuac");
+    /* **** Código de prueba *****/
     
-    coloniasDePrueba.add("Obrera");
-    coloniasDePrueba.add("Doctores");
-    coloniasDePrueba.add("Tránsito");
-    coloniasDePrueba.add("Pro hogar");
-    coloniasDePrueba.add("Anáhuac");
-   
+    EstadoRepublicaDAO estadoRepDao = new EstadoRepublicaIMPL();
+    mpioDao = new MunicipioIMPL();
+
+    estadosRep = estadoRepDao.buscarTodo();
+    edosRepSeleccionados = new ArrayList<>();
+
+    coloniasVisibles = new ArrayList<>();
+
+    mpiosDelEstadoRepSelec = new ArrayList<>();
+    mpiosVisibles = new ArrayList<>();
+    
+    mpiosSeleccionados = new ArrayList<>();
+    coloniasSeleccionadas = new ArrayList<>();
+
   }
 
-  public String getEstadosDePrueba() {
-    StringBuilder edp = new StringBuilder(estadosDePrueba.get(0));
-
-    for (int i = 1; i < estadosDePrueba.size(); i++) {
-      edp.append("\n");
-      edp.append(estadosDePrueba.get(i));
-    }
-    return edp.toString();
-  }
-
-  public void setEstadosDePrueba(List<String> estadosDePrueba) {
-    this.estadosDePrueba = estadosDePrueba;
-  }
+//  public String getEstadosDePrueba() {
+//    StringBuilder edp = new StringBuilder(estadosDePrueba.get(0));
+//
+//    for (int i = 1; i < estadosDePrueba.size(); i++) {
+//      edp.append("\n");
+//      edp.append(estadosDePrueba.get(i));
+//    }
+//    return edp.toString();
+//  }
+//
+//  public void setEstadosDePrueba(List<String> estadosDePrueba) {
+//    this.estadosDePrueba = estadosDePrueba;
+//  }
+//
+//  public List<String> getMpiosDePrueba() {
+//    return mpiosDePrueba;
+//  }
+//
+//  public void setMpiosDePrueba(List<String> mpiosDePrueba) {
+//    this.mpiosDePrueba = mpiosDePrueba;
+//  }
+//
+//  public List<String> autocompletarEstados() {
+//    estadosAutoCompletados.add(estadosDePrueba.get(0));
+//    estadosAutoCompletados.add(estadosDePrueba.get(estadosDePrueba.size() - 1));
+//    return estadosAutoCompletados;
+//  }
+//
+//  public List<String> getEstadosAutoCompletados() {
+//    return estadosAutoCompletados;
+//  }
+//
+//  public void setEstadosAutoCompletados(List<String> estadosAutoCompletados) {
+//    this.estadosAutoCompletados = estadosAutoCompletados;
+//  }
+//
+//  public List<String> getEstadosSeleccionados() {
+//    estadosSeleccionados = estadosDePrueba;
+//    return estadosSeleccionados;
+//  }
+//
+//  public void setEstadosSeleccionados(List<String> estadosSeleccionados) {
+//    this.estadosSeleccionados = estadosSeleccionados;
+//  }
+//
+//  public List<String> getMpiosSeleccionados() {
+//    return mpiosSeleccionados;
+//  }
+//
+//  public void setMpiosSeleccionados(List<String> mpiosSeleccionados) {
+//    this.mpiosSeleccionados = mpiosSeleccionados;
+//  }
+//
+//  public List<String> getMpiosAutoCompletados() {
+//    return mpiosAutoCompletados;
+//  }
+//
+//  public void setMpiosAutoCompletados(List<String> mpiosAutoCompletados) {
+//    this.mpiosAutoCompletados = mpiosAutoCompletados;
+//  }
+//
+//  public String getEstadoSeleccionado() {
+//    return estadoSeleccionado;
+//  }
+//
+//  public void setEstadoSeleccionado(String estadoSeleccionado) {
+//    this.estadoSeleccionado = estadoSeleccionado;
+//  }
+//
+//  public List<String> getColoniasDePrueba() {
+//    return coloniasDePrueba;
+//  }
+//
+//  public void setColoniasDePrueba(List<String> coloniasDePrueba) {
+//    this.coloniasDePrueba = coloniasDePrueba;
+//  }
+//
+//  public List<String> getColoniasAutoCompletadas() {
+//    return coloniasAutoCompletadas;
+//  }
+//
+//  public void setColoniasAutoCompletadas(List<String> coloniasAutoCompletadas) {
+//    this.coloniasAutoCompletadas = coloniasAutoCompletadas;
+//  }
+//
+//  public List<String> getColoniasSeleccionadas() {
+//    return coloniasSeleccionadas;
+//  }
+//
+//  public void setColoniasSeleccionadas(List<String> coloniasSeleccionadas) {
+//    this.coloniasSeleccionadas = coloniasSeleccionadas;
+//  }
   
-  public List<String> getMpiosDePrueba() {
-    return mpiosDePrueba;
-  }
-
-  public void setMpiosDePrueba(List<String> mpiosDePrueba) {
-    this.mpiosDePrueba = mpiosDePrueba;
-  }
-
-  public List<String> autocompletarEstados() {
-    estadosAutoCompletados.add(estadosDePrueba.get(0));
-    estadosAutoCompletados.add(estadosDePrueba.get(estadosDePrueba.size()-1));
-    return estadosAutoCompletados;
-  }
-
-  public List<String> getEstadosAutoCompletados() {
-    return estadosAutoCompletados;
-  }
-
-  public void setEstadosAutoCompletados(List<String> estadosAutoCompletados) {
-    this.estadosAutoCompletados = estadosAutoCompletados;
-  }
-
-  public List<String> getEstadosSeleccionados() {
-    estadosSeleccionados = estadosDePrueba;
-    return estadosSeleccionados;
-  }
-
-  public void setEstadosSeleccionados(List<String> estadosSeleccionados) {
-    this.estadosSeleccionados = estadosSeleccionados;
-  }
-
-  public List<String> getMpiosSeleccionados() {
-    return mpiosSeleccionados;
-  }
-
-  public void setMpiosSeleccionados(List<String> mpiosSeleccionados) {
-    this.mpiosSeleccionados = mpiosSeleccionados;
-  }
-
-  public List<String> getMpiosAutoCompletados() {
-    return mpiosAutoCompletados;
-  }
-
-  public void setMpiosAutoCompletados(List<String> mpiosAutoCompletados) {
-    this.mpiosAutoCompletados = mpiosAutoCompletados;
-  }
-
   public String getNombre() {
     return nombre;
   }
@@ -137,39 +260,90 @@ public class ZonasVistaBean implements Serializable {
   public void onEstadosChange() {
   }
 
-  public String getEstadoSeleccionado() {
-    return estadoSeleccionado;
+  public List<EstadoRepublica> getEstadosRep() {
+    return estadosRep;
   }
 
-  public void setEstadoSeleccionado(String estadoSeleccionado) {
-    this.estadoSeleccionado = estadoSeleccionado;
+  public void setEstadosRep(List<EstadoRepublica> estadosRep) {
+    this.estadosRep = estadosRep;
   }
 
-  public List<String> getColoniasDePrueba() {
-    return coloniasDePrueba;
+  public List<Municipio> getMpiosVisibles() {
+    mpiosVisibles = mpioDao.buscarMunicipiosPorEstado(edosRepSeleccionados.get(idEdoVisible).getIdEstado());
+    return mpiosVisibles;
   }
 
-  public void setColoniasDePrueba(List<String> coloniasDePrueba) {
-    this.coloniasDePrueba = coloniasDePrueba;
+  public void setMpiosVisibles(List<Municipio> mpiosVisibles) {
+    this.mpiosVisibles = mpiosVisibles;
   }
 
-  public List<String> getColoniasAutoCompletadas() {
-    return coloniasAutoCompletadas;
+  public List<Colonia> getColoniasVisibles() {
+    return coloniasVisibles;
   }
 
-  public void setColoniasAutoCompletadas(List<String> coloniasAutoCompletadas) {
-    this.coloniasAutoCompletadas = coloniasAutoCompletadas;
+  public void setColoniasVisibles(List<Colonia> coloniasVisibles) {
+    this.coloniasVisibles = coloniasVisibles;
   }
 
-  public List<String> getColoniasSeleccionados() {
-    return coloniasSeleccionados;
+  public List<EstadoRepublica> getEdosRepSeleccionados() {
+    return edosRepSeleccionados;
   }
 
-  public void setColoniasSeleccionados(List<String> coloniasSeleccionados) {
-    this.coloniasSeleccionados = coloniasSeleccionados;
+  public void setEdosRepSeleccionados(List<EstadoRepublica> edosRepSelec) {
+    this.edosRepSeleccionados = edosRepSelec;
   }
+
+  public int getIdEdoVisible() {
+    return idEdoVisible;
+  }
+
+  public void setIdEdoVisible(int idEdoVisible) {
+    this.idEdoVisible = idEdoVisible;
+  }
+
+  public EstadoRepublica getEdoRepVisible() {
+    return edoRepVisible;
+  }
+
+  public void setEdoRepVisible(EstadoRepublica edoRepVisible) {
+    this.edoRepVisible = edoRepVisible;
+  }
+
+  public List<Municipio> getMpiosDelEstadoRepSelec() {
+    return mpiosDelEstadoRepSelec;
+  }
+
+  public void setMpiosDelEstadoRepSelec(List<Municipio> mpiosDelEstadoRepSelec) {
+    this.mpiosDelEstadoRepSelec = mpiosDelEstadoRepSelec;
+  }
+
+  public void onMpiosChange() {
+  }
+
+  public List<Colonia> getColoniasDelEstadoRepSelec() {
+    return coloniasDelEstadoRepSelec;
+  }
+
+  public void setColoniasDelEstadoRepSelec(List<Colonia> coloniasDelEstadoRepSelec) {
+    this.coloniasDelEstadoRepSelec = coloniasDelEstadoRepSelec;
+  }
+
+  public List<Municipio> getMpiosSeleccionados() {
+    return mpiosSeleccionados;
+  }
+
+  public void setMpiosSeleccionados(List<Municipio> mpiosSeleccionados) {
+    this.mpiosSeleccionados = mpiosSeleccionados;
+  }
+
+  public List<Colonia> getColoniasSeleccionadas() {
+    return coloniasSeleccionadas;
+  }
+
+  public void setColoniasSeleccionadas(List<Colonia> coloniasSeleccionadas) {
+    this.coloniasSeleccionadas = coloniasSeleccionadas;
+  }
+
   
   
-  
-
 }
